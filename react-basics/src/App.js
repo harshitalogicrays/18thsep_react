4c608f36be99390f19e29e68af118739c6c7ab76
harshita <harshita@logicrays.com>
2024-12-18 09:39:11 +0530

    First Commit

---
+++ react-basics/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  let name = "Happy"
+
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <h2>Welcome to LRA</h2>
+      <h2>Name = {name} </h2>
+    </div>
+  );
+}
+
+export default App;
